cc5218d3487605d67056a5738a82875363c61eeb
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-12-01 16:42:25 -0500

    Update header

--- enhanced-evil-paredit.el
+++ enhanced-evil-paredit.el
@@ -25,11 +25,20 @@
 ;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; The **enhanced-evil-paredit** package prevents parenthesis imbalance when
-;; using *evil-mode* with *paredit*. It intercepts *evil-mode* commands such as
-;; delete, change, and paste, blocking their execution if they would break the
-;; parenthetical structure. This guarantees that your Lisp code remains
-;; syntactically correct while retaining the editing features of *evil-mode*.
+;; The enhanced-evil-paredit package prevents parenthesis imbalance when using
+;; evil-mode with paredit.
+;;
+;; It intercepts evil-mode commands such as delete, change, and paste, blocking
+;; their execution if they would break the parenthetical structure. This
+;; guarantees that your Lisp code remains syntactically correct while retaining
+;; the editing features of evil-mode.
+;;
+;; Installation:
+;; -------------
+;; (use-package enhanced-evil-paredit
+;;   :ensure t
+;;   :config
+;;   (add-hook 'paredit-mode-hook #'enhanced-evil-paredit-mode))
 
 ;;; Code:
 
